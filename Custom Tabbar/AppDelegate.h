2938f4cea8830fbbf2cbbf6c46485bb4cec76834
Karthick <karthick.c@technoduce.com>
2017-11-21 16:54:27 +0530

    Initial Commit

---
+++ Custom Tabbar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Custom Tabbar
+//
+//  Created by Karthick on 11/21/17.
+//  Copyright © 2017 Karthick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Custom Tabbar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Custom Tabbar
+//
+//  Created by Karthick on 11/21/17.
+//  Copyright © 2017 Karthick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
